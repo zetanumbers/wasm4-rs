74390f243edfeab213bc40e2ed7b12f008f1efec
Daria Sukhonina <dariasukhonina@gmail.com>
2022-01-02 16:56:59 +0300

    fix(docs): copy example's source into wasm4 docs

--- wasm4/src/lib.rs
+++ wasm4/src/lib.rs
@@ -2,7 +2,28 @@
 //! # Examples
 //!
 //! ```no_run
-#![doc = include_str!("../../examples/count/src/main.rs")]
+//! #![no_main]
+//!
+//! struct MyRuntime {
+//!     count: i32,
+//! }
+//!
+//! // prints "tick..." every second
+//! impl wasm4::Runtime for MyRuntime {
+//!     fn start(_: wasm4::Resources) -> Self {
+//!         MyRuntime { count: 0 }
+//!     }
+//!
+//!     fn update(&mut self) {
+//!         if self.count % 60 == 0 {
+//!             wasm4::trace("tick");
+//!             self.count = 0;
+//!         }
+//!         self.count += 1;
+//!     }
+//! }
+//!
+//! wasm4::main! { MyRuntime }
 //! ```
 
 pub mod draw;
