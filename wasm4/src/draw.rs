883fd55ae101d7da61c8a0dd163eae3494bc8463
Daria Sukhonina <dariasukhonina@gmail.com>
2022-01-08 01:27:44 +0300

    feat: add `blit` support

--- wasm4/src/draw.rs
+++ wasm4/src/draw.rs
@@ -1,3 +1,4 @@
+use crate::utils::{checked_add_pairs, le_pairs, lt_pairs};
 use core::{cell::Cell, marker::PhantomData, mem};
 
 pub struct Framebuffer(PhantomData<*mut ()>);
@@ -43,5 +44,184 @@ impl Framebuffer {
 
     pub fn text(&self, text: &str, start: [i32; 2]) {
         unsafe { wasm4_sys::textUtf8(text.as_ptr(), text.len(), start[0], start[1]) }
+    }
+
+    pub fn blit(&self, sprite: &impl Blit, start: [i32; 2], transform: BlitTransform) {
+        sprite.blit(start, transform, self)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Sprite<Bytes: ?Sized> {
+    shape: [u32; 2],
+    bpp: BitsPerPixel,
+    bytes: Bytes,
+}
+
+impl<const N: usize> Sprite<[u8; N]> {
+    pub const fn from_bytes(bytes: [u8; N], shape: [u32; 2], bpp: BitsPerPixel) -> Option<Self> {
+        if match shape[0].checked_mul(shape[1]) {
+            Some(it) => it as usize,
+            None => return None,
+        } < match N.checked_shl(3 - bpp as u32) {
+            Some(it) => it,
+            None => return None,
+        } {
+            Some(Sprite { shape, bpp, bytes })
+        } else {
+            None
+        }
+    }
+
+    pub const unsafe fn from_bytes_unchecked(
+        bytes: [u8; N],
+        shape: [u32; 2],
+        bpp: BitsPerPixel,
+    ) -> Self {
+        Sprite { shape, bpp, bytes }
+    }
+}
+
+impl Sprite<[u8]> {
+    /// Get the sprite's shape.
+    pub const fn shape(&self) -> [u32; 2] {
+        self.shape
+    }
+
+    /// Get the sprite's width.
+    pub const fn width(&self) -> u32 {
+        self.shape[0]
+    }
+
+    /// Get the sprite's height.
+    pub const fn height(&self) -> u32 {
+        self.shape[1]
+    }
+
+    /// Get the sprite's bpp (bits per pixel).
+    pub const fn bpp(&self) -> BitsPerPixel {
+        self.bpp
+    }
+
+    /// Get a reference to the sprite's bytes.
+    pub const fn bytes(&self) -> &[u8] {
+        &self.bytes
+    }
+
+    /// Create a subview to the sprite. Returns `None` if subview is out of bounds of the sprite.
+    pub const fn view<'a>(&'a self, start: [u32; 2], shape: [u32; 2]) -> Option<SpriteView<'a>> {
+        if lt_pairs(start, self.shape)
+            && le_pairs(
+                match checked_add_pairs(start, shape) {
+                    Some(it) => it,
+                    None => return None,
+                },
+                self.shape,
+            )
+        {
+            Some(SpriteView {
+                sprite: self,
+                start,
+                shape,
+            })
+        } else {
+            None
+        }
+    }
+
+    /// Create a subview to the sprite. Does not perform in bounds checks.
+    ///
+    /// ## Safety
+    ///
+    /// Resulting subview should be inside of the bounds of the `Sprite`
+    pub const unsafe fn view_unchecked<'a>(
+        &'a self,
+        start: [u32; 2],
+        shape: [u32; 2],
+    ) -> SpriteView<'a> {
+        SpriteView {
+            sprite: self,
+            start,
+            shape,
+        }
+    }
+}
+
+#[repr(u32)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum BitsPerPixel {
+    One,
+    Two,
+}
+
+bitflags::bitflags! {
+    #[derive(Default)]
+    pub struct BlitTransform: u32 {
+        const FLIP_X = 0b0010;
+        const FLIP_Y = 0b0100;
+        const ROTATE = 0b1000;
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct SpriteView<'a> {
+    sprite: &'a Sprite<[u8]>,
+    start: [u32; 2],
+    shape: [u32; 2],
+}
+
+impl<'a> SpriteView<'a> {
+    /// Get the view's underlying sprite.
+    pub fn sprite(&self) -> &Sprite<[u8]> {
+        self.sprite
+    }
+
+    /// Get the sprite view's start.
+    pub fn start(&self) -> [u32; 2] {
+        self.start
+    }
+
+    /// Get the sprite view's shape.
+    pub fn shape(&self) -> [u32; 2] {
+        self.shape
+    }
+}
+
+pub trait Blit {
+    fn blit(&self, start: [i32; 2], transform: BlitTransform, _framebuffer: &Framebuffer);
+}
+
+impl Blit for Sprite<[u8]> {
+    fn blit(&self, start: [i32; 2], transform: BlitTransform, _framebuffer: &Framebuffer) {
+        let flags = self.bpp as u32 | transform.bits();
+        unsafe {
+            wasm4_sys::blit(
+                self.bytes.as_ptr(),
+                start[0],
+                start[1],
+                self.shape[0],
+                self.shape[1],
+                flags,
+            )
+        }
+    }
+}
+
+impl Blit for SpriteView<'_> {
+    fn blit(&self, start: [i32; 2], transform: BlitTransform, _framebuffer: &Framebuffer) {
+        let flags = self.sprite.bpp as u32 | transform.bits();
+        unsafe {
+            wasm4_sys::blitSub(
+                self.sprite.bytes.as_ptr(),
+                start[0],
+                start[1],
+                self.shape[0],
+                self.shape[1],
+                self.start[0],
+                self.start[1],
+                self.sprite.shape[0],
+                flags,
+            )
+        }
     }
 }
--- wasm4/src/utils.rs
+++ wasm4/src/utils.rs
@@ -10,3 +10,24 @@ impl fmt::Display for OutOfDomainError {
 }
 
 impl std::error::Error for OutOfDomainError {}
+
+pub(crate) const fn checked_add_pairs(lhs: [u32; 2], rhs: [u32; 2]) -> Option<[u32; 2]> {
+    Some([
+        match lhs[0].checked_add(rhs[0]) {
+            Some(it) => it,
+            None => return None,
+        },
+        match lhs[1].checked_add(rhs[1]) {
+            Some(it) => it,
+            None => return None,
+        },
+    ])
+}
+
+pub(crate) const fn lt_pairs(lhs: [u32; 2], rhs: [u32; 2]) -> bool {
+    lhs[0] < rhs[0] && lhs[1] < rhs[1]
+}
+
+pub(crate) const fn le_pairs(lhs: [u32; 2], rhs: [u32; 2]) -> bool {
+    lhs[0] <= rhs[0] && lhs[1] <= rhs[1]
+}
