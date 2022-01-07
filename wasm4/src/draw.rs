fe8a08a0a307e194a7bbfdd2ba94a6855bf13032
Daria Sukhonina <dariasukhonina@gmail.com>
2022-01-08 01:19:54 +0300

    refactor!: `Framebuffer`'s methods

--- wasm4/src/draw.rs
+++ wasm4/src/draw.rs
@@ -11,32 +11,32 @@ impl Framebuffer {
     pub const HEIGHT: usize = 160;
     pub const BYTE_LENGTH: usize = Self::WIDTH * Self::HEIGHT / 4;
 
-    pub fn get(&self) -> &Cell<[u8; Self::BYTE_LENGTH]> {
+    pub fn as_cell(&self) -> &Cell<[u8; Self::BYTE_LENGTH]> {
         // SAFETY: WASM-4 is single-threaded
         unsafe { mem::transmute(wasm4_sys::FRAMEBUFFER) }
     }
 
-    pub fn line(&self, x1: i32, y1: i32, x2: i32, y2: i32) {
-        unsafe { wasm4_sys::line(x1, y1, x2, y2) }
+    pub fn line(&self, start: [i32; 2], end: [i32; 2]) {
+        unsafe { wasm4_sys::line(start[0], start[1], end[0], end[0]) }
     }
 
-    pub fn hline(&self, x: i32, y: i32, len: u32) {
-        unsafe { wasm4_sys::hline(x, y, len) }
+    pub fn hline(&self, start: [i32; 2], len: u32) {
+        unsafe { wasm4_sys::hline(start[0], start[1], len) }
     }
 
-    pub fn vline(&self, x: i32, y: i32, len: u32) {
-        unsafe { wasm4_sys::vline(x, y, len) }
+    pub fn vline(&self, start: [i32; 2], len: u32) {
+        unsafe { wasm4_sys::vline(start[0], start[1], len) }
     }
 
-    pub fn oval(&self, x: i32, y: i32, width: u32, height: u32) {
-        unsafe { wasm4_sys::oval(x, y, width, height) }
+    pub fn oval(&self, start: [i32; 2], shape: [u32; 2]) {
+        unsafe { wasm4_sys::oval(start[0], start[1], shape[0], shape[1]) }
     }
 
-    pub fn rect(&self, x: i32, y: i32, width: u32, height: u32) {
-        unsafe { wasm4_sys::rect(x, y, width, height) }
+    pub fn rect(&self, start: [i32; 2], shape: [u32; 2]) {
+        unsafe { wasm4_sys::rect(start[0], start[1], shape[0], shape[1]) }
     }
 
-    pub fn text(&self, text: &str, x: i32, y: i32) {
-        unsafe { wasm4_sys::textUtf8(text.as_ptr(), text.len(), x, y) }
+    pub fn text(&self, text: &str, start: [i32; 2]) {
+        unsafe { wasm4_sys::textUtf8(text.as_ptr(), text.len(), start[0], start[1]) }
     }
 }
