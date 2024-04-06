2bf32d15e5a8e923892a8bcb3c9046f21afbe721
Daria Sukhonina <dariasukhonina@gmail.com>
2024-04-06 14:02:28 +0300

    docs: include examples into documentation with `include_str!` macro

--- wasm4/src/draw.rs
+++ wasm4/src/draw.rs
@@ -1,41 +1,7 @@
 //! # Examples
 //!
 //! ```ignore
-//! #![no_main]
-//!
-//! use wasm4 as w4;
-//!
-//! struct SansRuntime {
-//!     framebuffer: w4::draw::Framebuffer,
-//! }
-//!
-//! // displays sans
-//! impl w4::rt::Runtime for SansRuntime {
-//!     fn start(res: w4::rt::Resources) -> Self {
-//!         SansRuntime {
-//!             framebuffer: res.framebuffer,
-//!         }
-//!     }
-//!
-//!     fn update(&mut self) {
-//!         w4::include_sprites! {
-//!             // 0xa64902 is a background color (indexed 0)
-//!             // 0x000000 is a foreground color (indexed 1)
-//!             // every other color is added if needed
-//!             const PALETTE: _ = common_palette!(0xa64902, 0x000000);
-//!             // every image may contain at most 4 colors each (transparent included)
-//!             // all images may contain at most 4 color total (transparent excluded)
-//!             const SMILE: _ = include_sprite!("src/sans.png");
-//!             const TALK: _ = include_sprite!("src/talk.png");
-//!         };
-//!
-//!         self.framebuffer.replace_palette(PALETTE);
-//!         self.framebuffer.blit(&SMILE, [68, 32], <_>::default());
-//!         self.framebuffer.blit(&TALK, [0, 92], <_>::default());
-//!     }
-//! }
-//!
-//! w4::main! { SansRuntime }
+#![doc = include_str!("../examples/sans/main.rs")]
 //! ```
 //!
 //! ![hey there kiddo](https://raw.githubusercontent.com/ZetaNumbers/wasm4-rs/00e582199ed13e59153b808126e4a5ab74267a31/examples/sans/preview.png "sans")
--- wasm4/src/lib.rs
+++ wasm4/src/lib.rs
@@ -1,30 +1,7 @@
 //! # Examples
 //!
 //! ```no_run
-//! #![no_main]
-//!
-//! use wasm4 as w4;
-//!
-//! struct MyRuntime {
-//!     count: i32,
-//! }
-//!
-//! // prints "tick" every second
-//! impl w4::rt::Runtime for MyRuntime {
-//!     fn start(_: w4::rt::Resources) -> Self {
-//!         MyRuntime { count: 0 }
-//!     }
-//!
-//!     fn update(&mut self) {
-//!         if self.count % 60 == 0 {
-//!             w4::trace("tick");
-//!             self.count = 0;
-//!         }
-//!         self.count += 1;
-//!     }
-//! }
-//!
-//! w4::main! { MyRuntime }
+#![doc = include_str!("../examples/ticking.rs")]
 //! ```
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
